47515c9a6dc5fbd4411b5e7b24f0f2a552be6641
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-11-03 15:01:35 +0900

    :sparkles: 필드 추가 제작

--- signup.php
+++ signup.php
@@ -3,8 +3,11 @@ $conn = mysqli_connect('localhost', 'test', '1111', 'test');
 $name = $_POST['name'];
 $id = $_POST['id'];
 $pw = $_POST['pw'];
+$gender = $_POST['gender'];
+$nikname = $_POST['nikname'];
+$email = $_POST['email'];
 
-$sql = "insert into user(name, id, pw) values('$name','$id','$pw')";
+$sql = "insert into user(name, id, pw, gender, nikname, email) values('$name','$id','$pw', '$gender', '$nikname', '$email')";
 $result = mysqli_query($conn, $sql);
 
 if($result) echo "회원가입 완료";
