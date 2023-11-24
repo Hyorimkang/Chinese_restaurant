40661be031e2fef83241a2c595125a18552df116
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2023-11-24 14:47:04 +0900

    :lipstick: 리뷰작성 페이지 제작

--- login.php
+++ login.php
@@ -4,17 +4,16 @@ $conn = mysqli_connect('localhost', 'test', '1111', 'test');
 $id = $_POST['id'];
 $pw = $_POST['pw'];
 
-$sql = "select id from where id='$id'";
+$sql = "select id from user where id='$id'";
 $result = mysqli_query($conn, $sql);
 
-if($id!=$result) {
-    $sql = "insert into user(name, id, pw, gender, nikname, email) values('$name','$id','$pw', '$gender', '$nikname', '$email')";
-    $result = mysqli_query($conn, $sql);
+if($id==$result) {echo "로그인 완료";
+    //$sql = "select from chinese_menu(id, pw) values('$id', '$pw')";
+    //$result = mysqli_query($conn, $sql);
 
-    if($result) echo "회원가입 완료";
-    else echo "회원가입 실패";
+    // if($result) 
+    // else echo "로그인 실패";
 }
-else echo "중복된 아이디로 회원가입이 불가능 합니다.";
 
 mysqli_close($conn);
 ?>
