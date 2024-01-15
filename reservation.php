5d702daa5e1998b880ca6aff5e142fd7c17b724a
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2024-01-15 16:13:42 +0900

    :sparkles: 회원가입 제작 완료

--- reservation.php
+++ reservation.php
@@ -2,6 +2,14 @@
 $conn = mysqli_connect('localhost', 'test', '1111', 'test');
 $date = $_POST['date'];
 $menu = $_POST['menu'];
+$cnt = $_POST['cnt'];
 $id = $_POST['id'];
 
+$sql="insert into reservation('id', 'date', 'menu', cnt) values('$id', '$date', '$menu', '$cnt')";
+$result = mysqli_query($conn, $sql);
+
+if($result){
+    echo "<script>alert(\"예약이 완료되었습니다.\")</script>";
+}
+mysqli_close($sql);
 ?>
--- signup.php
+++ signup.php
@@ -14,7 +14,10 @@ if(!$result) {
     $sql = "insert into user(name, id, pw, gender, nikname, email) values('$name','$id','$pw', '$gender', '$nikname', '$email')";
     $result = mysqli_query($conn, $sql);
 
-    if($result) echo "회원가입 완료";
+    if($result){
+        echo "<script>alert('예약이 완료되었습니다.')</script>";
+        echo "<script>location.replace('./login.html')</script>";
+    }
     else echo "회원가입 실패";
 }
 else echo "중복된 아이디로 회원가입이 불가능 합니다.";
