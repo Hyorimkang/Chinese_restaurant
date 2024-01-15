153e0d8b392f3467019f502ef2011e8ea17cd4cd
강효림 <103922285+Hyorimkang@users.noreply.github.com>
2024-01-15 17:08:28 +0900

    :sparkles: 로그인 구현완료:

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+
 $conn = mysqli_connect('localhost', 'test', '1111', 'test');
 
 $id = $_POST['id'];
@@ -7,13 +8,19 @@ $pw = $_POST['pw'];
 $sql = "select id from user where id='$id'";
 $result = mysqli_query($conn, $sql);
 
-if($id==$result) {echo "로그인 완료";
-    //$sql = "select from chinese_menu(id, pw) values('$id', '$pw')";
-    //$result = mysqli_query($conn, $sql);
+$sql = "select * from user where id = '$id' and pw = '$pw'";
+$result = mysqli_fetch_array(mysqli_query($conn, $sql));
 
-    // if($result) 
-    // else echo "로그인 실패";
+if($result){
+    session_start();
+    $_SESSION['user_id'] = $result['id'];
+    $_SESSION['user_pw'] = $result['pw'];
+    echo "<script>alert('로그인이 완료되었습니다.')</script>";
+    echo "<script>location.replace('./home.html')</script>";
 }
+else
+    echo "<script>alert('로그인을 실패했습니다.')</script>";
+    echo "<script>location.replace('./login.html')</script>";
 
 mysqli_close($conn);
 ?>
